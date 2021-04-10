976b2d14db239ce14e1421a2ff7221867735503d
svalenciasan <52926410+svalenciasan@users.noreply.github.com>
2021-04-10 18:30:03 -0500

    Updated move, part of attack.

--- Project_Bat/Assets/Scripts/PlayerController.cs
+++ Project_Bat/Assets/Scripts/PlayerController.cs
@@ -8,20 +8,19 @@ public class PlayerController : MonoBehaviour
     
     private PlayerInputActions playerInput;
     private Rigidbody2D rb;
+    private float moveDirection;
+
     [SerializeField] float speed = 10f;
+
+    [SerializeField] float attackRange = 1.2f;
+    [SerializeField] Transform attackPoint;
+    [SerializeField] LayerMask ball;
 
     private void Awake()
     {
         playerInput = new PlayerInputActions();
         rb = GetComponent<Rigidbody2D>();
     }
-
-    private void FixedUpdate()
-    {
-        Vector2 moveInput = playerInput.Player.Move.ReadValue<Vector2>();
-        rb.velocity = moveInput * speed;
-    }
-
     private void OnEnable()
     {
         playerInput.Enable();
@@ -32,9 +31,25 @@ public class PlayerController : MonoBehaviour
         playerInput.Disable();
     }
 
-    private void ProcessInput()
+    private void FixedUpdate()
     {
+        playerInput.Player.Move.performed += ctx => Move(ctx.ReadValue<Vector2>());
+        playerInput.Player.Move.canceled += ctx => Move(Vector2.zero);
+    }
 
+    private void Move(Vector2 input)
+    {
+        rb.velocity = (input.normalized) * speed;
+    }
+
+    private void Attack()
+    {
+        Collider2D[] hit = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, ball);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 
     private void OnAnimatorMove()
